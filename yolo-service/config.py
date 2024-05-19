9335271a048c8d664ab8d3528e921c86193b1830
Donaldcho <donaldcho28@gmail.com>
2024-05-19 14:10:50 +0700

    Donald add modules

--- yolo-service/config.py
+++ yolo-service/config.py
@@ -1,18 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 CONFIG = {
-    "confidence_threshold": 0.5,
-    "max_cosine_distance": 0.4,
-    "max_reconnect_attempts": 5,
-    "max_age": 30,
-    "n_init": 3,
+    "confidence_threshold": float(os.getenv('CONFIDENCE_THRESHOLD', 0.5)),
+    "max_cosine_distance": float(os.getenv('MAX_COSINE_DISTANCE', 0.4)),
+    "max_reconnect_attempts": int(os.getenv('MAX_RECONNECT_ATTEMPTS', 5)),
+    "max_age": int(os.getenv('MAX_AGE', 30)),
+    "n_init": int(os.getenv('N_INIT', 3)),
     "rabbitmq_host": os.getenv('RABBITMQ_HOST', 'localhost'),
     "rabbitmq_port": int(os.getenv('RABBITMQ_PORT', 5672)),
     "rabbitmq_queue": os.getenv('RABBITMQ_QUEUE', 'yolo_triggers'),
-    "camera_index": int(os.getenv('CAMERA_INDEX', 0)),
-    "SECRET_KEY": os.getenv('SECRET_KEY', 'your_secret_key')
+    "camera_index": int(os.getenv('CAMERA_INDEX', 1))
 }
--- yolo-service/feature_extractor.py
+++ yolo-service/feature_extractor.py
@@ -1,7 +1,6 @@
 import torch
 
 def load_feature_extractor():
-    """Loads the feature extractor model (ResNet)."""
     feature_extractor = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
     feature_extractor = torch.nn.Sequential(*(list(feature_extractor.children())[:-1]))
     feature_extractor.eval()
